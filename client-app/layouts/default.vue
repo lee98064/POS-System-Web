43d9984423b5a35890ba546e96e23824739ce9d2
Ryan Lee <lee98064@gmail.com>
2022-02-20 18:05:26 +0800

    Add Dashboard page and i18n plugins

--- client-app/layouts/default.vue
+++ client-app/layouts/default.vue
@@ -1,64 +1,67 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app :clipped="false" fixed>
+      <v-img
+      height="140"
+      class="pa-4"
+      src="https://upload.cc/i1/2022/02/20/aZvYlH.png"
     >
+      <div class="text-center">
+        <v-avatar class="mb-4" color="grey darken-1" size="64">
+          <v-img
+            aspect-ratio="30"
+            src="https://yt3.ggpht.com/esazPAO03T0f0vKdByJvkDy6MSwjyG5f-c_2S2CJapszQ3KPQyZarpoqvgv0Us0atUbILytj=s88-c-k-c0x00ffffff-no-rj"
+          />
+        </v-avatar>
+        <h2 class="white--text">Demo User</h2>
+      </div>
+    </v-img>
+    <v-divider></v-divider>
+
+      <v-divider></v-divider>
+
       <v-list>
         <v-list-item
-          v-for="(item, i) in items"
-          :key="i"
-          :to="item.to"
-          router
-          exact
+          v-for="item in links"
+          :key="item.icon"
+          link
+          :to="item.path"
         >
-          <v-list-item-action>
+          <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
+          </v-list-item-icon>
+
           <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
+            <v-list-item-title>{{ $t(item.name) }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
+      <template v-slot:append>
+        <div class="pa-2">
+          <v-select
+            :items="optionLang"
+            v-model="$store.state.lang"
+            item-text="text"
+            item-value="value"
+            filled
+            label="語言"
+            dense
+            @change="setLang($store.state.lang)"
+          ></v-select>
+        </div>
+      </template>
     </v-navigation-drawer>
-    <v-app-bar :clipped-left="clipped" fixed app>
+    <v-app-bar fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>mdi-{{ `chevron-${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>mdi-application</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>mdi-minus</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+      <v-toolbar-title>{{ $t('title') }}</v-toolbar-title>
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>mdi-menu</v-icon>
-      </v-btn>
     </v-app-bar>
+
     <v-main>
-      <v-container>
+      <v-container class="py-8 px-6" fluid>
         <Nuxt />
       </v-container>
     </v-main>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light> mdi-repeat </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -67,26 +70,30 @@ export default {
   name: 'DefaultLayout',
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
+      drawer: true,
+      links: [
         {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/',
+          name: 'Sidebar.dashboard',
+          icon: 'mdi-monitor-dashboard',
+          path: '/dashboard',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire',
+          name: 'Send',
+          icon: 'mdi-send',
+          path: '/send',
         },
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js',
+      optionLang: [
+        { text: '中文', value: 'zh', id: 1 },
+        { text: 'English', value: 'en', id: 2 },
+      ],
     }
+  },
+  methods: {
+    setLang(value) {
+      this.$store.commit('setLang', value)
+      this.$i18n.locale = value
+    },
   },
 }
 </script>
---
+++ client-app/store/index.js
@@ -0,0 +1,10 @@
+import Vuex from 'vuex'
+import { state } from './state.js'
+import { mutations } from './mutations.js'
+const createStore = () => {
+  return new Vuex.Store({
+    state,
+    mutations,
+  })
+}
+export default createStore
